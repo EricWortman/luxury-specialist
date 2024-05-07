1be394023dacb8c48cadb0a5b9bfb1e5cf4335f2
Ewortman <you@example.com>
2024-05-07 15:21:59 -0500

    update all

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const images = [
-      "lux_photos/ferrari_1.jpg",
+      "lux_photos/ferrari_16.jpg",
       "lux_photos/ferrari_2.jpg",
       "lux_photos/ferrari_3.jpg",
       "lux_photos/ferrari_4.jpg",
       "lux_photos/ferrari_5.jpg",
       "lux_photos/ferrari_6.jpg",
       "lux_photos/ferrari_7.jpg",
+      "lux_photos/ferrari_12.jpg",
+      "lux_photos/ferrari_13.jpg",
+      "lux_photos/ferrari_14.jpg",
 
-      // Add more image filenames as needed
+    
     ];
   
     const banner = document.getElementById("banner");
@@ -19,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function() {
       currentIndex = (currentIndex + 1) % images.length;
     }
   
-    rotateBanner(); // Initial rotation
+    rotateBanner(); 
   
     setInterval(rotateBanner, 5000); // Rotate every 5 seconds (adjust as needed)
   });
